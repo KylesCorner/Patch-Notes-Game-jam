dae945a247c8f262b337f11eae7bdc1300ec9589
KylesCorner <kylekrstulich@gmail.com>
2025-09-13 15:24:10 -0600

    added steering wheel

---
+++ Assets/SteeringWheel.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SteeringWheel : MonoBehaviour
+{
+    public float turnSpeed = 250f;
+    public float maxTurnAngle = 360f;
+
+    private float currentAngle = 0f;
+    private float steerInput = 0f;
+
+    private PlayerInputActions inputActions;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+        inputActions.Player.Steer.performed += ctx => steerInput = ctx.ReadValue<float>();
+        inputActions.Player.Steer.canceled += ctx => steerInput = 0f;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
+
+    private void Update()
+    {
+        if (steerInput == 0f)
+        {
+            currentAngle = Mathf.Lerp(currentAngle, 0f, Time.deltaTime * 3f); // smooth return
+        }
+        
+        currentAngle += steerInput * turnSpeed * Time.deltaTime;
+        currentAngle = Mathf.Clamp(currentAngle, -maxTurnAngle, maxTurnAngle);
+
+        transform.localRotation = Quaternion.Euler(0f, 0f, -currentAngle);
+    }
+}
