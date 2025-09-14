a6b38175d4f1a447afee2019cb900cfb8f891a0e
KaelanC-TechPriest <der-tech-priest@proton.me>
2025-09-14 15:06:04 -0600

    added background to scene

--- Assets/RoadlinesController.cs
+++ Assets/RoadlinesController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class RoadlinesController : MonoBehaviour
 {
     public RoadController roadController;
-    private Vector3 vehicleVelocity = Vector3.down;
+    private Vector3 vehicleVelocity = Vector3.down * 2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,7 +19,7 @@ public class RoadlinesController : MonoBehaviour
         transform.position = new Vector3(roadController.transform.position.x, transform.position.y + vehicleVelocity.y * Time.deltaTime, 0);
 
         Vector3 viewportPos = Camera.main.WorldToViewportPoint(transform.position);
-        if (viewportPos.y < -0.1f || viewportPos.y > 1.1f || viewportPos.x < -0.1f || viewportPos.x > 1.1f)
+        if (viewportPos.y < -0.3f || viewportPos.y > 1.1f || viewportPos.x < -0.1f || viewportPos.x > 1.1f)
         {
             Destroy(gameObject);
         }
